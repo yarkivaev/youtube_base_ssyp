4a166dbd7acebe69723b803f66632ff22bc3c1c0
ksamyshkin <190073633+ksamyshkin@users.noreply.github.com>
2025-07-17 11:55:05 +0700

    Password hashing

--- src/main/java/ru/ssyp/youtube/sqlite/SqliteUsers.java
+++ src/main/java/ru/ssyp/youtube/sqlite/SqliteUsers.java
@@ -3,11 +3,18 @@ package ru.ssyp.youtube.sqlite;
 import ru.ssyp.youtube.User;
 import ru.ssyp.youtube.Users;
 
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
 import java.sql.*;
+import java.util.Arrays;
 import java.util.Base64;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Pattern;
 
 public class SqliteUsers implements Users {
     private final Map<String, User> sessions;
@@ -26,15 +33,41 @@ public class SqliteUsers implements Users {
         }
     }
 
+    private byte[] pbkdf2(String password, byte[] salt) {
+        KeySpec spec = new PBEKeySpec(password.toCharArray(), salt, 65536, 128);
+
+        try {
+            SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+            return factory.generateSecret(spec).getEncoded();
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     private String hashPassword(String password) {
-        // TODO
-        return password;
+        SecureRandom random = new SecureRandom();
+        byte[] salt = new byte[16];
+        random.nextBytes(salt);
+
+        Base64.Encoder encoder = Base64.getUrlEncoder().withoutPadding();
+        return encoder.encodeToString(salt) + "$" + encoder.encodeToString(pbkdf2(password, salt));
+    }
+
+    private boolean checkPassword(String hash, String password) {
+        Base64.Decoder decoder = Base64.getUrlDecoder();
+        String[] parts = hash.split(Pattern.quote("$"), 1);
+
+        byte[] salt = decoder.decode(parts[0]);
+        byte[] hashBytes = decoder.decode(parts[1]);
+
+        return Arrays.equals(pbkdf2(password, salt), hashBytes);
     }
 
     private String genToken() {
         SecureRandom random = new SecureRandom();
         byte[] bytes = new byte[20];
         random.nextBytes(bytes);
+
         Base64.Encoder encoder = Base64.getUrlEncoder().withoutPadding();
         return encoder.encodeToString(bytes);
     }
@@ -60,12 +93,16 @@ public class SqliteUsers implements Users {
     @Override
     public String login(String name, String password) {
         try {
-            PreparedStatement statement = conn.prepareStatement("SELECT * FROM user WHERE name = ? AND passhash = ?;");
+            PreparedStatement statement = conn.prepareStatement("SELECT passhash FROM user WHERE name = ?;");
             statement.setString(1, name);
-            statement.setString(2, hashPassword(password));
             ResultSet rs = statement.executeQuery();
 
             if (!rs.next()) {
+                return null;
+            }
+
+            String hash = rs.getString("passhash");
+            if (!checkPassword(hash, password)) {
                 return null;
             }
         } catch (SQLException e) {
