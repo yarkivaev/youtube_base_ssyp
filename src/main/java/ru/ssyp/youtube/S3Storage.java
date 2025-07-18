4a5ba6c75eab1413aa7bc1e382c58ca4b4436cc3
Участник олимпиады <programmer@main.nsu.ru>
2025-07-18 17:39:33 +0700

    S3Storage v1

--- src/main/java/ru/ssyp/youtube/S3Storage.java
+++ src/main/java/ru/ssyp/youtube/S3Storage.java
@@ -1,20 +1,117 @@
 package ru.ssyp.youtube;
+import io.minio.*;
+import io.minio.errors.*;
 
-import java.io.File;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 import java.io.InputStream;
 
 public class S3Storage implements Storage {
 
+    private final MinioClient minioClient;
+
+    private final String bucketName;
+
+    public S3Storage(MinioClient minioClient, String bucketName) {
+        this.minioClient = minioClient;
+        this.bucketName = bucketName;
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        MinioClient minioClient =
+                MinioClient.builder()
+                        .endpoint("http://localhost:9000")
+                        .credentials("minioadmin", "minioadmin")
+                        .build();
+        System.out.println(minioClient.listBuckets().get(0).name());
+
+        createBucketIfNotExists(minioClient);
+
+        S3Storage storage = new S3Storage(minioClient, "test-bucket");
+        storage.upload("new-test-file.txt", new ByteArrayInputStream("Hello world!!!!".getBytes()));
+        InputStream fileContent = storage.download("new-test-file.txt");
+        System.out.println(new String(fileContent.readAllBytes()));
+        minioClient.close();
+
+    }
+
     @Override
-    public void upload(String name, InputStream stream) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'upload'");
+    public void upload(String name, InputStream inputStream) {
+        ObjectWriteResponse objectWriteResponse = null;
+        try {
+            objectWriteResponse = minioClient.putObject(PutObjectArgs
+                    .builder()
+                    .bucket(bucketName)
+                    .object(name)
+                    .stream(inputStream, -1, 1024 * 1024 * 5)
+                    .build());
+        } catch (
+                ErrorResponseException
+                | InsufficientDataException
+                | InternalException
+                | InvalidKeyException
+                | InvalidResponseException
+                | IOException
+                | NoSuchAlgorithmException
+                | ServerException
+                | XmlParserException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("File uploaded successfully." + objectWriteResponse.object());
     }
 
     @Override
     public InputStream download(String name) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'download'");
+        try {
+            Files.deleteIfExists(Paths.get(name));
+            minioClient.downloadObject(
+                    DownloadObjectArgs.builder()
+                            .bucket(bucketName)
+                            .object(name)
+                            .filename(name)
+                            .build());
+            return new FileInputStream(name);
+        }
+            catch (
+                    ErrorResponseException
+                            | InsufficientDataException
+                            | InternalException
+                            | InvalidKeyException
+                            | InvalidResponseException
+                            | IOException
+                            | NoSuchAlgorithmException
+                            | ServerException
+                            | XmlParserException e) {
+                throw new RuntimeException(e);
+            }
+
+        }
+
+    private static void createBucketIfNotExists(MinioClient minioClient)
+            throws ErrorResponseException, InsufficientDataException, InternalException,
+            InvalidKeyException, InvalidResponseException, IOException,
+            NoSuchAlgorithmException, ServerException, XmlParserException {
+
+        boolean found = minioClient.bucketExists(BucketExistsArgs.builder()
+                .bucket("test-bucket")
+                .build());
+
+        if (!found) {
+            minioClient.makeBucket(MakeBucketArgs.builder()
+                    .bucket("test-bucket")
+                    .build());
+            System.out.println("Bucket '" + "test-bucket" + "' created successfully.");
+        }
     }
-    
+
 }
+
+
+
+
+
+
