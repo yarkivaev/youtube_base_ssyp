14f512967a665baa09e2c15fec606b60e137453c
ksamyshkin <190073633+ksamyshkin@users.noreply.github.com>
2025-07-22 16:37:01 +0700

    implement ServerYoutube

--- src/main/java/ru/ssyp/youtube/ServerYoutube.java
+++ src/main/java/ru/ssyp/youtube/ServerYoutube.java
@@ -3,30 +3,38 @@ package ru.ssyp.youtube;
 import ru.ssyp.youtube.users.Session;
 import ru.ssyp.youtube.video.Video;
 import ru.ssyp.youtube.video.VideoMetadata;
+import ru.ssyp.youtube.video.Videos;
 
 import java.io.IOException;
 import java.io.InputStream;
 
 public class ServerYoutube implements Youtube {
+    private final Youtube youtube;
+    private final Videos videos;
 
+    public ServerYoutube(Youtube youtube, Videos videos) {
+        this.youtube = youtube;
+        this.videos = videos;
+    }
 
     @Override
     public Video videoInfo(int videoId) {
-        return null;
+        return videos.video(videoId);
     }
 
     @Override
     public Video[] videos() {
-        return new Video[0];
+        return videos.videos();
     }
 
     @Override
     public void upload(Session user, VideoMetadata metadata, InputStream stream) throws IOException, InterruptedException {
-
+        youtube.upload(user, metadata, stream);
+        videos.addNew(user, metadata);
     }
 
     @Override
     public InputStream load(int videoId, int startSegment, int resolution) {
-        return null;
+        return youtube.load(videoId, startSegment, resolution);
     }
 }
--- src/main/java/ru/ssyp/youtube/sqlite/SqliteVideos.java
+++ src/main/java/ru/ssyp/youtube/sqlite/SqliteVideos.java
@@ -1,17 +1,23 @@
 package ru.ssyp.youtube.sqlite;
 
+import ru.ssyp.youtube.users.Session;
 import ru.ssyp.youtube.video.Video;
 import ru.ssyp.youtube.video.VideoMetadata;
 import ru.ssyp.youtube.video.Videos;
 
 public class SqliteVideos implements Videos {
     @Override
-    public int addNew(VideoMetadata metadata) {
+    public int addNew(Session session, VideoMetadata metadata) {
         return 0;
     }
 
     @Override
     public Video video(int videoId) {
         return null;
+    }
+
+    @Override
+    public Video[] videos() {
+        return new Video[0];
     }
 }
