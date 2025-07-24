04ec19e4fdf0a5dd6918a864d6307448d743cd2f
Участник олимпиады <programmer@main.nsu.ru>
2025-07-24 11:15:06 +0700

    new server code

--- src/main/java/ru/ssyp/youtube/server/Server.java
+++ src/main/java/ru/ssyp/youtube/server/Server.java
@@ -45,7 +45,7 @@ public class Server {
         Socket socket = serverSocket.accept();
         System.out.println("accepted");
         while (true) {
-            //Socket socket = serverSocket.accept();
+//            Socket socket = serverSocket.accept();
             byte[] shortByteBuffer = new byte[1];
             byte[] intByteBuffer = new byte[4];
             InputStream inputStream = socket.getInputStream();
@@ -168,7 +168,6 @@ public class Server {
             //inputStream.close();
             //outputStream.close();
             //socket.close();
-            // -> возврат результата клиенту
         }
 
     }
@@ -212,7 +211,6 @@ public class Server {
         }
         return newArray;
     }
-    //0 1 0 0 start = 1 length = 4
 
     public static void main(String[] args) throws SQLException, IOException, InvalidTokenException {
         System.out.println("Starting the server");
@@ -230,53 +228,3 @@ public class Server {
         ).serve();
     }
 }
-//=======
-//        while (true) {
-//            Socket socket = serverSocket.accept();
-//            byte[] shortByteBuffer = new byte[1];
-//            byte[] intByteBuffer = new byte[4];
-//            InputStream inputStream = socket.getInputStream();
-//            OutputStream outputStream = socket.getOutputStream();
-//            inputStream.read(shortByteBuffer);
-//            int intCommand = IntCodec.byteToInt_1(shortByteBuffer);
-//            Command command = null;
-//            if (intCommand == 0x01) {
-//                inputStream.read(intByteBuffer);
-//                int videoId = IntCodec.byteToInt(intByteBuffer);
-//
-//                inputStream.read(intByteBuffer);
-//                int segmentId = IntCodec.byteToInt(intByteBuffer);
-//                outputStream.write(segmentId);
-//
-//                inputStream.read(shortByteBuffer);
-//                int quality = IntCodec.byteToInt_1(shortByteBuffer);
-//
-//                command = new GetVideoSegment(videoId , segmentId, quality, youtube);
-//                byte[] videoSegment = command.act().readAllBytes();
-//                outputStream.write(IntCodec.intToByte(videoSegment.length));
-//                outputStream.write(videoSegment);
-//            }
-//            if (intCommand == 0x02) {
-//                command = new ListVideosCommand(youtube);
-//                outputStream.write(command.act().readAllBytes());
-//            }
-//        }
-//    }
-//
-//    public static void main(String[] args) throws SQLException, IOException, InvalidTokenException {
-//        System.out.println("Starting the server");
-//        ServerSocket serverSocket = new ServerSocket(8080);
-//        Users users = new SqliteUsers(
-//            new SqliteDatabase(
-//                DriverManager.getConnection("jdbc:sqlite::memory:")
-//            ),
-//            new TokenGenRandomB64(20)
-//        );
-//        new Server(
-//            serverSocket,
-//            new ScreamingYoutube(),
-//            users
-//>>>>>>> 6867e1af0be7a3aa3687561dacc887683567ebd0
-//        ).serve();
-//    }
-//}
